a436408c92230d8cae14bfd29d043f4f06aff439
lucasdze <lucasdze@gmail.com>
2022-08-09 16:51:57 -0500

    test gitleaks action

---
+++ .history/entrypoint_20220809163145.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CONFIG=""
+# check if using gitleaks config or not
+if [ -f "$GITHUB_WORKSPACE/.gitleaks.toml" ]
+then
+  CONFIG=" --config=$GITHUB_WORKSPACE/.gitleaks.toml"
+fi
+
+echo running gitleaks "$(gitleaks --version) with the following command👇"
+
+DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
+
+if [ "$GITHUB_EVENT_NAME" = "push" ]
+then
+  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
+  gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
+elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
+then 
+  git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
+  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+  gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+  rm commit_list.txt
+fi 
+
+if [ $? -eq 1 ]
+then
+  echo -e "\e[31m🛑 STOP! Gitleaks encountered leaks"
+  echo "----------------------------------"
+  echo -e $DONATE_MSG
+  exit 1
+else
+  echo -e "\e[32m✅ SUCCESS! Your code is good to go!"
+  echo "------------------------------------"
+  echo -e $DONATE_MSG
+fi
+
---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+  <img alt="gitleaks" src="https://raw.githubusercontent.com/zricethezav/gifs/master/gitleakslogo.png" height="70" />
+</p>
+
+Gitleaks Action provides a simple way to run gitleaks in your CI/CD pipeline.
+
+
+### Sample Workflow
+```
+name: gitleaks
+
+on: [push,pull_request]
+
+jobs:
+  gitleaks:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v1
+    - name: gitleaks-action
+      uses: zricethezav/gitleaks-action@master
+```
+
+### Using your own .gitleaks.toml configuration
+Include a .gitleaks.toml in the root of your repo directory.
+
+### NOTE!!!
+You must use `actions/checkout` before the gitleaks-action step. If you are using `actions/checkout@v2` you must specify a commit depth other than the default which is 1. 
+
+ex: 
+```
+    steps:
+    - uses: actions/checkout@v2
+      with:
+        fetch-depth: '0'
+    - name: gitleaks-action
+      uses: zricethezav/gitleaks-action@master
+```
+
+using a fetch-depth of '0' clones the entire history. If you want to do a more efficient clone, use '2', but that is not guaranteed to work with pull requests.   
